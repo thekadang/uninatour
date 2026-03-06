8f5c23a0fe8c094685e6ce6efe2bfa48be92a3d5
thekadang <yokadang@nate.com>
2026-03-06 18:58:34 +0900

    Update repository url to uninatour

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/unina/',
+  base: '/uninatour/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
